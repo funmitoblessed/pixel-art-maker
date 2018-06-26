5ec1c9fb43b01a810beb986c9c56b4f83c3c37c7
ofunmito <funmito.odefemi@outlook.com>
2018-06-26 18:56:08 +0100

    feat: Add event listener for changes in inputWeight and inputHeight

--- designs.js
+++ designs.js
@@ -5,6 +5,27 @@
 
 function makeGrid() {
 
-// Your code goes here!
+    // Your code goes here!
+    // declare variables to collect value of table height and width
+    var gridHeight, gridWidth;
+    // event listener for a change in value of Grid Height
+    $('#inputHeight').on('change', function () {
+        gridHeight = $('#inputHeight').val();
+        // return gridHeight;
+        console.log(gridHeight);
+    });
+     // event listener for a change in value of Grid Width
+    $('#inputWeight').on('change', function () {
+        gridWidth = $('#inputWeight').val();
+        // return gridWidth;
+        console.log(gridWidth);
+    });
+    // event listener for the submit button and grid creator
+    $('#input').click(function () {
+        // $('#pixelCanvas').
+        console.log(gridHeight, gridWidth);
+    });
 
 }
+
+makeGrid();
